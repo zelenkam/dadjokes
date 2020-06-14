67dca7f080d346dcd6f771166da1e6dcf28358f3
Michaela Zelenkova <michaela.zelenka@gmail.com>
2020-06-14 19:05:25 +0200

    Snad hotovo, jdu si dát panáka :-)

---
+++ src/components/Joke.vue
@@ -0,0 +1,119 @@
+<template>
+  <div class="container">
+    <div class="joke">
+      <div class="joke__body">
+        <img class="joke__user" v-bind:src="`assets/img/${user}.png`" />
+        <p class="joke__text">{{text}}</p>
+      </div>
+      <div class="joke__likes">
+        <button id="btn-up" class="btn-like btn-like--up" @click="like"></button>
+        <span id="likes-up" class="likes-count likes-count--up">{{likes}}</span>
+        <button id="btn-down" class="btn-like btn-like--down" @click="dislike"></button>
+        <span id="likes-down" class="likes-count likes-count--down">{{dislikes}}</span>
+      </div>
+    </div>
+  </div>
+</template>
+
+
+<script>
+export default {
+  name: "Joke",
+  props: ["user", "text", "likes", "dislikes"],
+  data() {
+    return {
+      likes: this.likes,
+      dislikes: this.dislikes
+    };
+  },
+  methods: {
+    like() {
+      this.likes += 1;
+    },
+    dislike() {
+      this.dislikes += 1;
+    }
+  }
+};
+</script>
+
+<style>
+* {
+  box-sizing: border-box;
+}
+
+html {
+  font-family: sans-serif;
+}
+
+.container {
+  max-width: 30rem;
+  margin: 5rem auto;
+}
+
+.joke {
+  margin-bottom: 2rem;
+}
+
+.joke__body {
+  display: flex;
+}
+
+.joke__user {
+  flex: 0 0 auto;
+  width: 5rem;
+  height: 5rem;
+  margin-right: 1rem;
+}
+
+.joke__text {
+  background-color: cornflowerblue;
+  color: white;
+  border-radius: 1rem;
+  padding: 1.5rem 2rem;
+  margin: 0;
+  line-height: 150%;
+}
+
+.joke__likes {
+  display: flex;
+  justify-content: flex-end;
+  align-items: center;
+  margin-top: 1rem;
+}
+
+.likes-count {
+  font-size: 1.5rem;
+  font-weight: bold;
+  margin: 0 0.5rem;
+}
+
+.likes-count--up {
+  color: #00e268;
+}
+
+.likes-count--down {
+  color: #ff5654;
+}
+
+.btn-like {
+  border: none;
+  outline: none;
+  width: 2rem;
+  height: 2rem;
+  border-radius: 1rem;
+  background-size: 1.2rem;
+  background-position: center;
+  background-repeat: no-repeat;
+}
+
+.btn-like--up {
+  background-color: #00e268;
+  background-image: url("../img/like-up.png");
+}
+
+.btn-like--down {
+  background-color: #ff5654;
+  background-image: url("../img/like-down.png");
+}
+</style>
--- src/index.js
+++ src/index.js
@@ -1,60 +1,9 @@
-// import './index.html';
-// import './style.css';
+import Vue from 'vue/dist/vue.js';
 
-
-
-// let upLikes = 0;
-// let downLikes = 0;
-
-// const btnUp = document.querySelector('#btn-up');
-// const btnDown = document.querySelector('#btn-down');
-
-// btnUp.addEventListener('click', () => {
-//   upLikes += 1;
-//   const likesUpElm = document.querySelector('#likes-up');
-//   likesUpElm.textContent = upLikes;
-// });
-
-// btnDown.addEventListener('click', () => {
-//   downLikes += 1;
-//   const likesDownElm = document.querySelector('#likes-down');
-//   likesDownElm.textContent = downLikes;
-// });
-
-// new VTTCue({
-//   el: '#app',
-//   data: {
-//     upLikes: 0,
-//     downLikes: 0,
-
-//   },
-//   methods: {
-//     like() {
-//       console.log('ahoj')
-//       this.upLikes += 1;
-//     },
-//     dislike() {
-//       this.downLikes -= 1;
-//     },
-//   }
-
-// })
+import App from './components/App.vue';
+import './index.html';
 
 new Vue({
   el: '#app',
-  data: {
-    upLikes: 0,
-    downLikes: 0,
-
-  },
-  methods: {
-    like() {
-      console.log('ahoj')
-      this.upLikes += 1;
-    },
-    dislike() {
-      this.downLikes += 1;
-    },
-  }
-
-})
+  render: h => h(App),
+});
